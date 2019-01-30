5e047c87f94dc7fcc051bc7cbaadb02c5925f2ad
Yong Wang <wangyong820312@gmail.com>
2019-01-30 14:44:09 +0000

    test

--- frontend/server.js
+++ frontend/server.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 const proxy = httpProxy.createProxyServer();
 const app = express();
-
+// 
 app.use(express.static(path.join(__dirname, 'build')));
 
 const config = {
